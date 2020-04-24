278f754b951946c14a3da3ada92963ac314cb692
褚煦露 <chuxulu@lezhixing.com.cn>
2020-04-24 22:19:49 +0800

    firt exprience react module

---
+++ src/components/Box.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  border: 1px solid grey;
+  min-width: 300px;
+  min-height: 300px;
+  box-shadow: 0 0 3px rgba(0,0,0,0.5);
+`;
+
+export default Box;
