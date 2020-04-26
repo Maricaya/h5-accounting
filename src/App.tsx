f8dbd0aeafb601b3da20e77c9fd7db09017fb197
Maricaya <915270549@qq.com>
2020-04-26 22:34:35 +0800

    添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {HashRouter as Router, Link, Route, Switch, Redirect} from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from 'components/Nav';
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -14,52 +15,28 @@ const Main = styled.div`
   overflow: auto;
 `
 
-const Nav = styled.nav`
-  border: 1px solid blue;
-  > ul {
-    display: flex;
-    > li {
-      width: 33.333%;
-      text-align: center;
-      padding: 16px;
-    }
-  }
-`
-
 function App() {
   return (
     <Router>
       <Wrapper>
         <Main>
           <Switch>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/money">
-            <Money />
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          <Redirect exact from="/" to="money" />
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页面</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav />
       </Wrapper>
     </Router>
   );
