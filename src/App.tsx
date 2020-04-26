e3474bbaeb4071118f8e347c37a6f196f5250d30
褚煦露 <chuxulu@lezhixing.com.cn>
2020-04-26 21:44:48 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,84 @@
 import React from 'react';
 import './App.css';
-import Button from "components/Button";
-import Box from "components/Box";
+import {HashRouter as Router, Link, Route, Switch, Redirect} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
+
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`
 
 function App() {
   return (
-    <Box>
-      <Button>按钮</Button>
-    </Box>
-  )
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页面</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
+  );
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function NoMatch() {
+  return <h2>404 页面不存在</h2>;
 }
 
 export default App;
