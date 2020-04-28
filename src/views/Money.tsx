197d62ded799086dfc0724a1823fe1101f14abf6
Maricaya <915270549@qq.com>
2020-04-28 22:04:30 +0800

    Money.tsx 模块化

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,124 +1,10 @@
 import Layout from 'components/Layout';
 import React from 'react';
 import styled from 'styled-components';
-
-const TagsSection = styled.section`
-  background: #fff;
-  padding: 12px 16px;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-  > ol { 
-    margin: 0 -12px;
-    > li { 
-      background: #D9D9D9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
-    }
-  }
-  > button {
-    background: none;
-    border: none;
-    padding: 2px 4px;
-    border-bottom: 1px solid #333;
-    color: #666;
-    margin-top: 8px;
-  }
-`
-
-const NotesSection = styled.section`
-  background: #f5f5f5;
-  padding: 0 16px;
-  font-size: 14px;
-  > label {
-    display:flex;
-    align-items: center;
-    > span { margin-right: 16px; white-space: nowrap; }
-    > input {
-      display:block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
-`
-const CategorySection = styled.section`
-  font-size: 24px;
-  > ul{
-    display:flex;
-    background:#c4c4c4;
-    > li {
-      width: 50%; 
-      text-align:center;
-      padding: 16px 0;
-      position:relative;
-      &.selected::after{
-        content: '';
-        display:block; 
-        height: 3px;
-        background:#333;
-        position:absolute;
-        bottom:0;
-        width: 100%;
-        left: 0;
-      }
-    }
-  }
-`
-const NumberPadSection = styled.section`
-  display:flex;
-  flex-direction: column;
-  > .output{
-    background:white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align:right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                inset 0 5px 5px -5px rgba(0,0,0,0.25);
-  }
-  > .pad{ 
-    > button{
-      font-size: 18px; float: left; width: 25%; height: 64px; border: none;
-      &.ok{ height: 128px; float: right; }
-      &.zero{ width: 50%; }
-      &:nth-child(1){
-        background:#f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background:#E0E0E0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background:#D3D3D3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background:#C1C1C1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background:#B8B8B8;
-      }
-      &:nth-child(12) {
-        background:#9A9A9A;
-      }
-      &:nth-child(14) {
-        background:#A9A9A9;
-      }
-    }
-  }
-`
+import {CategorySection} from './Money/CategorySection';
+import {NotesSection} from './Money/NotesSection';
+import {NumberPadSection} from './Money/NumberPadSection';
+import {TagsSection} from './Money/TagsSection';
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -126,21 +12,14 @@ const MyLayout = styled(Layout)`
 `
 
 function Money() {
-  return  (
+  return (
     <MyLayout>
       <TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
       </TagsSection>
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"  placeholder="在这里添加备注"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
---
+++ src/views/Money/TagsSection.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+  background: #fff;
+  padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  > ol { 
+    margin: 0 -12px;
+    > li { 
+      background: #D9D9D9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
+`
+export {TagsSection};
