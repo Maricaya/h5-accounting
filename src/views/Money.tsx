6324f920b86366193eb16475020c791bf5d5523f
褚煦露 <chuxulu@lezhixing.com.cn>
2020-04-28 11:21:19 +0800

    Money.tsx 之备注UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -27,7 +27,21 @@ const TagsSection = styled.section`
 `
 
 const NotesSection = styled.section`
-
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
 `
 const CategorySection = styled.section`
 
@@ -51,7 +65,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text" />
+          <input type="text"  placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
