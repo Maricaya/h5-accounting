7a315dcf4fed1b178cfbac5bbe8ec23d0b65b7d6
Maricaya <915270549@qq.com>
2020-04-28 11:13:46 +0800

    Money.txs 之 TagsSection

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,33 @@ import Tags from './views/Tags';
 import Statistics from './views/Statistics';
 import NoMatch from './views/NoMatch';
 import Money from './views/Money';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333;
+` 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Redirect exact from="/" to="money" />
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="money" />
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,84 @@
 import Layout from '../components/layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+  background: #fff;
+  padding: 12px 16px;
+  > ol { 
+    margin: 0 -12px;
+    > li { 
+      background: #D9D9D9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
+`
+
+const NotesSection = styled.section`
+
+`
+const CategorySection = styled.section`
+
+`
+const NumberPadSection = styled.section`
+
+`
 
 function Money() {
   return  (
     <Layout>
-      <h2>记账页</h2>
+      <TagsSection>
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
+        <button>新增标签</button>
+      </TagsSection>
+      <NotesSection>
+        <label>
+          <span>备注</span>
+          <input type="text" />
+        </label>
+      </NotesSection>
+      <CategorySection>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </CategorySection>
+      <NumberPadSection>
+        <div>100</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </NumberPadSection>
     </Layout>
   );
 }
