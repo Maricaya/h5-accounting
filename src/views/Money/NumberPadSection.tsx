3851780760b9c0b08a58f14b4b8b21175371632a
褚煦露 <chuxulu@lezhixing.com.cn>
2020-04-29 11:14:13 +0800

    数字面板按钮功能基本完成

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -46,12 +46,23 @@ const Wrapper = styled.section`
       &:nth-child(14) {
         background:#A9A9A9;
       }
+      &:active {
+        background-color: #666;
+      }
     }
   }
 `
 
 const NumberPadSection: React.FC = (props) => {
-  const [output, setOutput] = useState('0')
+  const [output, _setOutput] = useState('0')
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16)
+    } else if (output.length === 0) {
+      output = '0'
+    }
+    _setOutput(output)
+  }
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent
     if (text === null) {
@@ -76,9 +87,21 @@ const NumberPadSection: React.FC = (props) => {
         }
 
         break;
+      case '.':
+        if (output.indexOf('.') >= 0) {
+          return;
+        }
+        setOutput(output + '.')
+        break;
       case '删除':
+        if (output.length === 1) {
+          setOutput('')
+        }else {
+          setOutput(output.slice(0, -1))
+        }
         break;
       case '清空':
+        setOutput('0')
         break;
       case 'OK':
         break;
