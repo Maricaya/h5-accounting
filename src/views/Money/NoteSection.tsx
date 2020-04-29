acd5e4d449de8a99f3403c10f9e1238731b92f96
褚煦露 <chuxulu@lezhixing.com.cn>
2020-04-29 09:56:45 +0800

    完成 Category 切换功能

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
@@ -20,15 +20,35 @@ const Wrapper = styled.section`
 `
 
 const NoteSection: React.FC = (props) => {
+
   const [note, setNote] = useState('')
+  const refInput = useRef<HTMLInputElement>(null)
+
+  const onBlur = () => {
+    if (refInput.current !== null) {
+      setNote(refInput.current.value)
+    }
+  }
+
   return (
     <Wrapper>
       <label>
         <span>备注</span>
+        {/* 受控组件
         <input type="text" placeholder="在这里添加备注"
                value={note}
                onChange={(e) => setNote(e.target.value)}
+        />*/}
+        {/* 非受控组件 */}
+        <input type="text" placeholder="在这里添加备注"
+               ref={refInput}
+               defaultValue={note}
+               onBlur={onBlur}
         />
+      {/* React onChange 和 HTML onChange 是不一样的
+        React onChange 在你输入一个字的时候就触发
+        HTML onChange 在你鼠标一走的时候触发,早于 onBlur
+      */}
       </label>
     </Wrapper>
   )
