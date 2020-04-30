9e1b807d68c0e91487a351d57ba738417afbec67
褚煦露 <chuxulu@lezhixing.com.cn>
2020-04-30 11:06:23 +0800

    抽离 useTags 函数

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 
-/*  use 开头 hooks */
+/*  use 开头,封装一个自定义 Hook */
 const useTags = () => {
   const [tags, setTags] = useState<string[]>(['衣','食','住','行'])
   return {
