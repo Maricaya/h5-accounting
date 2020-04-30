c31f1c1fbd8b140ebdebf4719c0f92398257d6b1
Maricaya <915270549@qq.com>
2020-04-30 15:51:46 +0800

    tags 样式做完

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,16 +1,60 @@
 import Layout from 'components/Layout';
 import React from 'react';
 import {useTags} from 'useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: #fff;
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin: 0 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+  color: white;
+`
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+const Space = styled.div`
+  height: 16px;
+`
 
 function Tags() {
   const {tags, setTags} = useTags();
   console.log(setTags)
   return (
     <Layout>
-      <ol>
+      <TagList>
       {tags.map(tag =>
-        <li key={tag}>{tag}</li>)}
-      </ol>
+        <li key={tag}>
+          <span className="oneLine">{tag}很长很长很长很长很长很长很长很长很长很长很长很长很长</span>
+          <Icon name="right" />
+        </li>)
+      }
+      </TagList>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+          <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
