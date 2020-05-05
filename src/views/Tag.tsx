6dfb0352a81127f4e80d0b6ea3b984ea30a539c1
Maricaya <915270549@qq.com>
2020-05-05 23:54:09 +0800

    封装 findTag 到 useTags 里面

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import {useTags} from '../useTags';
+import { useParams } from 'react-router-dom';
+
+type Params = {
+  id: string
+}
 
 const Tag: React.FC = () => {
+  const {findTag} = useTags()
+  const { id } = useParams<Params>();
+  const tag = findTag(parseInt(id))
   return (<div>
-
+    {tag.name}
   </div>)
 }
 
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -46,8 +46,8 @@ function Tags() {
       <TagList>
       {tags.map(tag =>
         <li key={tag.id}>
-          <Link to={'/tags/' + tag}>
-          <span className="oneLine">{tag.name}</span>
+          <Link to={'/tags/' + tag.id}>
+          <span className="oneLine">{tag.id}:{tag.name}</span>
           <Icon name="right" />
           </Link>
         </li>)
