6b427ffb61320c222dcd7e5f9ad75da89af7d7bb
Maricaya <915270549@qq.com>
2020-05-05 22:44:16 +0800

    refactor: 将 tag:string 重构为 tag:{id:number;name:string}

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -35,8 +35,8 @@ const Wrapper = styled.section`
 `
 
 type Props = {
-  value: string[];
-  onChange: (selected: string[]) => void;
+  value: number[];
+  onChange: (selected: number[]) => void;
 }
 
 const TagsSection: React.FC<Props> = (props) => {
@@ -46,32 +46,33 @@ const TagsSection: React.FC<Props> = (props) => {
   // tags.map(tag => ... ) ts可以判断出tag是 String，而不是 Number 等等
   const {tags, setTags} = useTags()
   // const [setSelectedTags] = useState<string[]>([])
-  const selectedTags = props.value;
+  const selectedTagIds = props.value;
   const onAddTag = () => {
     const tagName = window.prompt('新标签的名称为')
     if (tagName !== null) {
-      setTags([...tags, tagName])
+      setTags([...tags, {id: Math.random(), name: tagName}])
     }
   }
-  const onToggleTag = (tag: string) => {
-    const index = selectedTags.indexOf(tag)
+  const onToggleTag = (tagId: number) => {
+    const index = selectedTagIds.indexOf(tagId)
     if (index >= 0) {
       // 如果 tag 已经被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
       // react 不推荐直接修改（vue可以
-      props.onChange(selectedTags.filter(t => t !== tag))
+      props.onChange(selectedTagIds.filter(t => t !== tagId))
     } else {
-      props.onChange([...selectedTags, tag])
+      props.onChange([...selectedTagIds, tagId])
     }
   }
-  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+  const getClass = (tagId: number) => selectedTagIds.indexOf(tagId) >= 0 ? 'selected' : ''
 
   return (
     <Wrapper>
       <ol>
-        {tags.map(tag => <li key={tag} onClick={
-          () => onToggleTag(tag)
-        } className={getClass(tag)}
-        >{tag}</li>)}
+        {tags.map(tag =>
+          <li key={tag.id} onClick={() => onToggleTag(tag.id)}
+              className={getClass(tag.id)}
+        >{tag.name}</li>
+        )}
       </ol>
       <button onClick={onAddTag}>新增标签</button>
     </Wrapper>
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -40,15 +40,14 @@ const Space = styled.div`
 `
 
 function Tags() {
-  const {tags, setTags} = useTags();
-  console.log(setTags)
+  const {tags} = useTags();
   return (
     <Layout>
       <TagList>
       {tags.map(tag =>
-        <li key={tag}>
+        <li key={tag.id}>
           <Link to={'/tags/' + tag}>
-          <span className="oneLine">{tag}</span>
+          <span className="oneLine">{tag.name}</span>
           <Icon name="right" />
           </Link>
         </li>)
