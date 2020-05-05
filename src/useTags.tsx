fe538b6884cc03275ebd965d11c3c3c24f81e25e
褚煦露 <chuxulu@lezhixing.com.cn>
2020-05-05 22:44:16 +0800

    refactor: 将 tag:string 重构为 tag:{id:number;name:string}

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -2,7 +2,13 @@ import {useState} from 'react';
 
 /*  use 开头,封装一个自定义 Hook */
 const useTags = () => {
-  const [tags, setTags] = useState<string[]>(['衣','食','住','行'])
+  const [tags, setTags] = useState<{id:number;name:string}[]>([
+    {id: 1,name: '衣'},
+    {id: 2,name: '食'},
+    {id: 3,name: '住'},
+    {id: 4,name: '行'}
+  ])
+  // const tagIds = tags.map(item => item.id)
   return {
     tags, setTags
   }
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -15,7 +15,7 @@ type Category = '-' | '+'
 
 function Money() {
   const [selected, setSelected] = useState({
-    tags: [] as string[],
+    tagIds: [] as number[],
     note: '',
     category: '-' as Category,
     amount: 0
@@ -30,8 +30,8 @@ function Money() {
 
   return (
     <MyLayout>
-      <TagsSection value={selected.tags}
-                   onChange={(tags) => onChange({tags}) }/>
+      <TagsSection value={selected.tagIds}
+                   onChange={(tagIds) => onChange({tagIds}) }/>
       <NoteSection value={selected.note}
                    onChange={(note) => onChange({note})}
       />
